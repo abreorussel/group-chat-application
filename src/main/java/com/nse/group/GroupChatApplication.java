528c38270c7bd54e1416886496011b1f31071f0b
Russel Abreo <abreorussel@gmail.com>
2021-03-13 12:16:24 +0530

    Initial Commit

---
+++ src/main/java/com/nse/group/GroupChatApplication.java
@@ -0,0 +1,13 @@
+package com.nse.group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nse/group/GroupChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nse.group;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
