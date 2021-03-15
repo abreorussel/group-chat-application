adf0c3fb4088785a4c0decf925c5206f571e1d32
Sujit1997r <79976040+Sujit1997r@users.noreply.github.com>
2021-03-14 23:58:22 -0700

    Add files via upload

---
+++ src/main/java/com/nse/group/service/MessageService.java
@@ -0,0 +1,27 @@
+package com.nse.group.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nse.group.entity.Message;
+import com.nse.group.repository.IMessageRepository;
+
+@Service
+public class MessageService {
+
+	@Autowired
+	IMessageRepository repo;
+
+	public Message saveMessage(Message message) {
+		message.setSentAt(LocalDateTime.now());
+		return repo.save(message);
+	}
+
+	public List<Message> findMessages(int fromUser, int toUser) {
+		return repo.findByFromAndToUser(fromUser, toUser);
+	}
+
+}
